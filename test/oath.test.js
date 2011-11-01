a0903f6cf54128cb366998b1a3dce4ee1574caa0
Jake Luer <jake.luer@gmail.com>
2011-11-01 06:56:58 -0400

    tests rewritten in sherlock

--- test/oath.test.js
+++ test/oath.test.js
@@ -1,47 +1,46 @@
-var assert = require('assert'),
-    oath = require('oath');
+var Sherlock = require('sherlock')
+  , assert = Sherlock.assert;
 
-module.exports = {
-  'oath has version': function () {
+var oath = require('..');
+
+module.exports = new Sherlock.Investigation('Oath', function (test, done) {
+  
+  test('oath has version', function (test, done) {
     assert.isNotNull(oath.version);
-  },
-  'basic oath success': function() {
-    var n = 0;
-    var success = function (data) {
-      n++;
-      assert.eql(data, { doctor: 'who' });
-    };
-    
-    var failure = function () {
-      n--;
-      assert.fail();
-    };
+    done();
+  });
+  
+  test('basic oath success', function (test, done) {
+    var success = Sherlock.Spy(function (data) {
+          assert.deepEqual(data, { doctor: 'who' });
+        })
+      , failure = Sherlock.Spy();
     
     var promise = function(data) {
       var o = new oath();
-      setTimeout(function() { o.resolve(data); }, 100);
+      setTimeout(function() { o.resolve(data);  }, 100);
       setTimeout(function() { o.reject('too slow'); }, 300);
       return o;
     };
     
-    promise({ doctor: 'who' })
-      .then(success, failure);
+    // waiting, to ensure o.reject is not called
+    setTimeout(function() {
+      done();
+    }, 500);
+    
+    promise({ doctor: 'who' }).then(success, failure);
     
     this.on('exit', function () {
-      assert.equal(n, 1, 'the success function has been called twice');
+      assert.equal(success.calls.length, 1, 'the success function was only called once');
+      assert.equal(failure.called, false, 'fail was not called');
     });
-  },
-  'basic oath failure': function () {
-    var n = 0;
-    var success = function (data) {
-      n++;
-      assert.fail();
-    };
-    
-    var failure = function (data) {
-      n--;
-      assert.equal(data, 'pretending i\'m too slow');
-    };
+  });
+  
+  test('basic oath failure', function (test, done) {
+    var success = Sherlock.Spy()
+      , failure = Sherlock.Spy(function (data) {
+          assert.equal(data, 'pretending i\'m too slow');
+        });
     
     var promise = function(data) {
       var o = new oath();
@@ -50,24 +49,32 @@ module.exports = {
       return o;
     };
     
+    // waiting to ensure o.resolve is not called
+    setTimeout(function() {
+      done();
+    }, 500);
+    
     promise({ doctor: 'who' })
       .then(success) // if no failure object skip
       .then(success, failure);
+    
     this.on('exit', function () {
-      assert.equal(n, -1, 'i have successfully failed');
+      assert.equal(success.called, false, 'success was not called');
+      assert.equal(failure.calls.length, 1, ' i have successfully failed, once');
     });
-  },
-  'multiple then chains on success in order': function () {
+  });
+  
+  test('multiple then chains on success in order', function (test, done) {
     var n = 0;
     var success1 = function (data) {
       assert.equal(n, 0, 'success2 not called yet');
-      assert.eql(data, { doctor: 'who' });
+      assert.deepEqual(data, { doctor: 'who' });
       n++;
     };
     
     var success2 = function (data) {
       assert.equal(n, 1, 'success1 has been called');
-      assert.eql(data, { doctor: 'who' });
+      assert.deepEqual(data, { doctor: 'who' });
       n++;
     };
     
@@ -83,23 +90,28 @@ module.exports = {
       return o;
     };
     
+    setTimeout(function () {
+      done();
+    }, 500);
+    
     promise({ doctor: 'who' }).then(success1, failure).then(success2);
     
     this.on('exit', function () {
       assert.equal(n, 2, 'the both success functions have been called');
     });
-  },
-  'multiple then chains on failure in order': function () {
+  });
+  
+  test('multiple then chains on failure in order', function (test, done) {
     var n = 0;
     var failure1 = function (data) {
       assert.equal(n, 0, 'failure2 not called yet');
-      assert.eql(data, { doctor: 'who' });
+      assert.deepEqual(data, { doctor: 'who' });
       n++;
     };
     
     var failure2 = function (data) {
       assert.equal(n, 1, 'failure1 has been called');
-      assert.eql(data, { doctor: 'who' });
+      assert.deepEqual(data, { doctor: 'who' });
       n++;
     };
     
@@ -115,13 +127,38 @@ module.exports = {
       return o;
     };
     
+    setTimeout(function () {
+      done();
+    }, 500);
+    
     promise({ doctor: 'who' }).then(success, failure1).then(success, failure2);
     
     this.on('exit', function () {
       assert.equal(n, 2, 'the both failures functions have been called');
     });
-  },
-  'oath get helper with pop': function () {
+  });
+  
+  test('oath call helper', function (test, done) {
+    var doctor = new oath(),
+        who, n=0;
+        
+    doctor
+      .call('who', { time: 'lord' });
+    
+    setTimeout(function() {
+      doctor.resolve({
+        who: function(data) { n++; who = data; } 
+      });
+      done();
+    }, 100);
+    
+    this.on('exit', function() {
+      assert.equal(n, 1, 'callback called');
+      assert.deepEqual(who, { time: 'lord' });
+    });
+  });
+  
+  test('oath get helper with pop', function (test, done) {
     var doctor = new oath(),
         who, tardis, n=0;
 
@@ -135,30 +172,15 @@ module.exports = {
     
     setTimeout(function() {
       doctor.resolve({ doctor: 'who' });
+      done();
     },0);
     
     this.on('exit', function() {
       assert.equal(n, 2, 'both callbacks called');
       assert.equal(who, 'who');
-      assert.eql(tardis, { doctor: 'who' });
+      assert.deepEqual(tardis, { doctor: 'who' });
     });
-  },
-  'oath call helper': function () {
-    var doctor = new oath(),
-        who, n=0;
-        
-    doctor
-      .call('who', { time: 'lord' });
-    
-    setTimeout(function() {
-      doctor.resolve({
-        who: function(data) { n++; who = data; } 
-      });
-    }, 100);
-    
-    this.on('exit', function() {
-      assert.equal(n, 1, 'callback called');
-      assert.eql(who, { time: 'lord' });
-    });
-  }
-};
+  });
+  
+  done();
+});
